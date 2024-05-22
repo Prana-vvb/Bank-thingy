6cb8dc2a06e6c5b316d894f2c4a839e496bb216f
stringcheeseguy <149595245+Prana-vvb@users.noreply.github.com>
2024-05-22 22:18:02 +0530

    Final Bank.c

--- Bank.c
+++ Bank.c
@@ -2,87 +2,148 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct{
+typedef struct {
     int accNo;
     char name[100];
+    char accType[10];
     int pin;
     float bal;
 }account;
 
-void displayBalance(account *acc){
-    printf("Balance is: Rs. %.2f\n", acc->bal);
-}
+int lc = 0;
 
-void deposit(account *acc, float amount){
-    acc->bal += amount;
-}
-
-void withdraw(account *acc, float amount){
-    if(amount > acc->bal){
-        printf("You don't have Rs. %.2f in your account\n", amount);
+int read(FILE *fp, account *acc, int pin){
+    char line[100];
+    int count = 0;
+    while(fgets(line, 100, fp) != NULL){
+        acc->accNo = atoi(strtok(line, ","));
+        strcpy(acc->name, strtok(NULL, ","));
+        strcpy(acc->accType, strtok(NULL, ","));
+        acc->pin = atoi(strtok(NULL, ","));
+        acc->bal = atof(strtok(NULL, ","));
+        if(acc->pin == pin){
+            return 1;
+        }
     }
-    else{
+    return 0;
+}
+
+int write(FILE *fp, account acc){
+    int accNo;
+    char line[100];
+    long int pos;
+
+    rewind(fp);
+    while(fgets(line, 100, fp) != NULL){
+        pos = ftell(fp);
+
+        accNo = atoi(strtok(line, ","));
+        if(accNo == acc.accNo){
+            fseek(fp, pos, SEEK_SET);
+            fprintf(fp, "%d,%s,%s,%d,%.2f\n", acc.accNo, acc.name, acc.accType, acc.pin, acc.bal);
+            printf("Account modified successfully.\n");
+            return 0;
+        }
+    }
+
+    printf("Account not found.\n");
+    return 1;
+}
+
+void displayBalance(account acc) {
+    printf("Balance is: Rs. %.2f\n", acc.bal);
+}
+
+void deposit(account *acc, float amount) {
+    acc->bal += amount;
+    return;
+}
+
+void withdraw(account *acc, float amount) {
+    if (amount > acc->bal) {
+        printf("You don't have Rs. %.2f in your account\n", amount);
+    } else {
         acc->bal -= amount;
         printf("Rs. %.2f has been withdrawn from your account\n", amount);
     }
+    return;
 }
 
-void createAcc(account *acc, char *name, int pin, float amount){
-    static int accNoCounter = 1;
-    acc->accNo = accNoCounter++;
-    strcpy(acc->name, name);
-    acc->pin = pin;
-    acc->bal = amount;
+void createAcc(FILE *fp){
+    lc++;
+    char name[100];
+    char type[10];
+    int pin;
+    float amt;
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Enter account type: ");
+    scanf("%s", type);
+    printf("Enter a PIN: ");
+    scanf("%d", &pin);
+    printf("Enter initial deposit amount: Rs. ");
+    scanf("%f", &amt);
+
+    fprintf(fp, "%d,%s,%s,%d,%.2f\n", lc, name, type, pin, amt);
+    printf("New account created successfully!\n");
+    return;
+}
+
+int numAcc(FILE *fp) {
+    char line[100];
+    int count = 0;
+    while(fgets(line, sizeof(line), fp) != NULL){
+        char *token;
+        int elements = 0;
+        token = strtok(line, ",");
+        while (token != NULL) {
+            elements++;
+            token = strtok(NULL, ",");
+        }
+        if (elements == 5) {
+            count++;
+        }
+    }
+    rewind(fp);
+    return count;
 }
 
 int main(){
-    int numAccounts = 0;
-    account *accounts = malloc(numAccounts * sizeof(account));
+    FILE *fp = fopen("accounts.csv", "a+");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
 
+    lc = numAcc(fp);
+
+    account acc;
     int pin;
+
     printf("         =====Welcome To Bank!!=====\n");
     printf("=============================================\n");
     printf("Please sign in to your account by entering your PIN to continue or type 1 to create a new account\n");
     scanf("%d", &pin);
 
     if(pin == 1){
-        char name[100];
-        int newPin;
-        float initialAmount;
+        createAcc(fp);
 
-        printf("Enter your name: ");
-        scanf("%s", name);
-        printf("Enter a PIN: ");
-        scanf("%d", &newPin);
-        printf("Enter initial deposit amount: Rs. ");
-        scanf("%f", &initialAmount);
-
-        accounts = realloc(accounts, (numAccounts + 1) * sizeof(account));
-        createAcc(&accounts[numAccounts++], name, newPin, initialAmount);
-        printf("New account created successfully!\n");
-
-        pin = newPin;
+        printf("Please sign in to your account by entering your PIN to continue or type 1 to create a new account\n");
+        scanf("%d", &pin);
     }
 
-    int found = 0;
-    account *currentAccount;
-    for(int i = 0; i < numAccounts; i++){
-        if (pin == accounts[i].pin) {
-            currentAccount = &accounts[i];
-            found = 1;
-            break;
-        }
-    }
+    int found = read(fp, &acc, pin);
 
     if(!found){
         printf("Unknown PIN entered\n");
-        free(accounts);
+        fclose(fp);
         return 0;
     }
 
     while(1){
         printf("=============================================\n");
-        printf("         =====Account: %s=====\n", currentAccount->name);
+        printf("         =====Account: %s=====\n", acc.name);
         printf("Options:\n1) Check account balance\n2) Deposit\n3) Withdraw\n4) Exit\n");
         printf("Enter your choice: ");
         int choice;
@@ -90,28 +151,45 @@ int main(){
 
         switch(choice){
             case 1:
-                displayBalance(currentAccount);
+                displayBalance(acc);
                 break;
             case 2:
                 printf("Enter amount to deposit: Rs. ");
                 float depositAmount;
                 scanf("%f", &depositAmount);
-                deposit(currentAccount, depositAmount);
+                deposit(&acc, depositAmount);
                 printf("Rs. %.2f has been successfully deposited in your account!\n", depositAmount);
                 break;
             case 3:
                 printf("Enter amount to withdraw: Rs. ");
                 float withdrawAmount;
                 scanf("%f", &withdrawAmount);
-                withdraw(currentAccount, withdrawAmount);
+                withdraw(&acc, withdrawAmount);
                 break;
             case 4:
-                printf("            =====Exiting=====\n");
-                free(accounts);
-                return 0;
+                char ch;
+                printf("Save changes and exit?[y/n]: ");
+                scanf(" %c", &ch);
+                if(ch == 'y'){
+                    int s = write(fp, acc);
+                    if(s){
+                        printf("Save unsuccessful!\n");
+                        printf("            =====Exiting=====\n");
+                        fclose(fp);
+                        return s;
+                    }
+                    printf("Save successful!\n");
+                    printf("            =====Exiting=====\n");
+                    fclose(fp);
+                    return s;
+                }
+                break;
             default:
                 printf("         =====Unknown Option Entered=====\n\n");
                 break;
         }
     }
+
+    fclose(fp);
+    return 0;
 }
